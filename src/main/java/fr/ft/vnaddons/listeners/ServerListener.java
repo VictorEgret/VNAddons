a9a552daeeb45fc8bad2373f83e973fa4526da05
Victor <47831936+VictorEgret@users.noreply.github.com>
2022-08-09 21:34:17 +0200

    Random server icon fix

--- src/main/java/fr/ft/vnaddons/listeners/ServerListener.java
+++ src/main/java/fr/ft/vnaddons/listeners/ServerListener.java
@@ -8,12 +8,13 @@ import org.bukkit.util.CachedServerIcon;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 
 public class ServerListener implements Listener {
 
     private final ArrayList<CachedServerIcon> icons = new ArrayList<>();
-    private int icon = 0;
+    private final Random rand = new Random();;
 
     public ServerListener() {
         loadServerIcons();
@@ -35,17 +36,14 @@ public class ServerListener implements Listener {
         }
     }
 
-    private CachedServerIcon getServerIcon() {
-        CachedServerIcon currentIcon = icons.get(icon);
-        icon++;
-        if (icon == icons.size())
-            icon = 0;
-        return currentIcon;
-    }
-
     @EventHandler
     public void onPing(ServerListPingEvent event) {
-        if (!icons.isEmpty())
-            event.setServerIcon(getServerIcon());
+        if (!icons.isEmpty()) {
+            try {
+                event.setServerIcon(icons.get(rand.nextInt(icons.size())));
+            } catch (Exception exception) {
+
+            }
+        }
     }
 }
